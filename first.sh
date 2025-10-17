73efe1057ecb07ff56938f32a071c09ee1ee8dd0
hao wang <hao.wang@mdpi.com>
2025-10-17 12:15:55 +0800

    learning shell programming

---
+++ first.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a comment!
+# echo Hello World
+echo "Hello       World"
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello     World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
